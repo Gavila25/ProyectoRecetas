b8ee45fd884541d90293e2b31f4aa053c36a96c7
Gavila25 <ghostguille2000@gmail.com>
2022-05-11 17:33:10 +0100

    Se crea el caso de uso de Buscar restaurante, solo falta hacer los test #104

---
+++ logica/src/main/java/es/iesptodelacruz/proyectoRecetas/modelo/GestionRestaurantes.java
@@ -0,0 +1,13 @@
+package es.iesptodelacruz.proyectoRecetas.modelo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GestionRestaurantes {
+    private HashMap<String,Restaurante> lista=new HashMap<>();
+
+    public ArrayList<Restaurante> getLista(String nombre) {
+            ArrayList<Restaurante> busqueda=new ArrayList<>();
+        return busqueda;
+    }
+}
